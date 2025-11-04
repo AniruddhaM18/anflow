169630119fb153da7f31f2e5038fd43c0e2f05ae
Aniruddha <aniruddhamaradwar9@gmail.com>
2025-11-04 23:42:47 +0530

    add

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,15 +2,18 @@ import express from "express";
 import cookieParser from 'cookie-parser';
 import authRouter from "./routes/auth.routes";
 import workflowRouter from "./routes/workflow.routes";
-
+import credentialRouter from "./routes/credential.routes";
+import webhookRouter from "./routes/workflow.routes";
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
 
 app.use("/auth", authRouter);
 app.use("/dashboard", workflowRouter);
+app.use("/dashboard", credentialRouter);
+app.use("/dashboard", webhookRouter);
 
-
+// test route
 app.get("/", (req , res) => {
     res.send("Server is running")
 });
