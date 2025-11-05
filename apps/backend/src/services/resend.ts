4d0435547346fd18f9ddeb3d74a4103a18c3534f
Aniruddha <aniruddhamaradwar9@gmail.com>
2025-11-05 12:23:50 +0530

    mail service

--- apps/backend/src/services/resend.ts
+++ apps/backend/src/services/resend.ts
@@ -1,12 +1,65 @@
-import { Resend } from 'resend';
-import { RESEND_KEY } from '../config';
+import { prismaClient } from "@repo/db";
+import { Request, Response } from "express";
+import { Resend } from "resend";
 
-const resend = new Resend(RESEND_KEY);
+export const resendService = async(req:Request, res:Response) => {
+  try{
+    const userId = req.user?.id;
+    if(!userId){
+      res.status(401).json({
+        message: "Unauthorized"
+      });
+      return;
+    }
+    const { to, html, subject } = req.body;
+    if(!to || !html || !subject){
+      res.json(400).json({
+        message: "All fields required"
+      });
+      return
+    }
+    const getCredential = await prismaClient.credential.findFirst({
+      where:{
+        userId,
+        platform: "GMAIL"
+      }
+    });
+    if(!getCredential){
+      res.status(401).json({
+        message: "Credential not found"
+      });
+      return;
+    }
 
-resend.emails.send({
-  from: 'onboarding@resend.dev',
-  to: 'anisoft18@gmail.com',
-  subject: 'Hello World',
-  html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
-});
+    const { apiKey, fromEmail } = getCredential.data as any;
+    if(!apiKey || !fromEmail){
+      res.status(401).json({
+        message: "Missing credentials"
+      });
+      return;
+    }
 
+    const resend = new Resend(apiKey);
+    const { data, error } = await resend.emails.send({
+    from: fromEmail,
+    to: Array.isArray(to) ? to : [to],//resend requires array
+    subject: subject,
+    html:html,
+    });
+    
+    if(error){
+      res.status(500).json({
+        message: "Error", error
+      });
+      return;
+    }
+    res.json({
+      message:"Email sent successfully ", data
+    });
+    }catch(err){
+    console.log(err);
+    res.status(500).json({
+      message: "Server error"
+    });
+  }
+}
