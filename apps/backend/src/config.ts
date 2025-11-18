09426be7077447285a12ef311d9b67665f141f9a
Aniruddha <aniruddhamaradwar9@gmail.com>
2025-11-19 00:05:27 +0530

    update

--- apps/backend/src/config.ts
+++ apps/backend/src/config.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const RESEND_KEY = process.env.RESEND_KEY || "re_A1YTwfUq_27P77STxsT1qE8DV2R5VDnzh";
-export const APP_URL = process.env.APP_URL || "http://localhost:3000";
-export const JWT_SECRET = process.env.JWT_SECRET || "ANFLO18#ani";
+export const RESEND_KEY = process.env.RESEND_KEY;
+export const APP_URL = process.env.APP_URL;
+export const JWT_SECRET = process.env.JWT_SECRET;
